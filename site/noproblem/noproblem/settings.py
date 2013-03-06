9007e47cc9f4e546f31b3f9b5b77cdb08c5204c2
guigarfr <guigarfr@gmail.com>
2013-03-06 12:38:11 +0100

    dinamic template directory
    changed template directory in settings.py . now templates are in
    subfolders called "templates"

--- site/noproblem/noproblem/settings.py
+++ site/noproblem/noproblem/settings.py
@@ -107,8 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/tuzon/Desktop/noproblem_piloto/noproblem/mytemplates',
-    '/Users/guigarfr/Dropbox/AppDidactica/noproblem_piloto/noproblem/mytemplates',
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
