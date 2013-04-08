410f07f2116edbb386dcbdfdd19addf7d334c5e3
Paula <paulatuzon@gmail.com>
2013-04-08 11:45:53 +0200

    Revert "pequeños cambios para subir a alwaysdata"
    This reverts commit 8b3105e6485662836c84c290d007fde604bad078.

--- site/noproblem/noproblem/settings.py
+++ site/noproblem/noproblem/settings.py
@@ -13,8 +13,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'paulatuzon_noproblemdb',                      # Or path to database file if using sqlite3.
-        'USER': '65065_pauly',                      # Not used with sqlite3.
+        'NAME': 'noproblemdb',                      # Or path to database file if using sqlite3.
+        'USER': 'pauly',                      # Not used with sqlite3.
         'PASSWORD': 'gaudenciaenguidanos',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
