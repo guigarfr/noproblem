7500fddc605ccb38b1a483d09209e0ce580e5d4d
Paula <paulatuzon@gmail.com>
2013-05-04 20:34:30 +0200

    He añadido últimos eventos en el blog
    Para ello, he modificado bloggy.html, y también las vistas del blog,
    donde he incluído el modelo Evento y he editado la vista para que me
    diera los eventos pasados en lugar de los próximos. Parece que me hago
    mayor en esto de Django… ;-)

--- site/noproblem/noproblem/blog/views.py
+++ site/noproblem/noproblem/blog/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 from django.template import RequestContext
+from noproblem.agenda.models import Evento
+from django.template import Context
+import datetime
 
 def bloggy(request):
-    return render_to_response('bloggy.html', context_instance=RequestContext(request))
+	hoy=datetime.datetime.now().date()
+	ultimos_eventos = Evento.objects.filter(fecha_ini__lte=hoy).order_by('fecha_ini')[:5]
+	context = Context({
+					  'ultimos_eventos':ultimos_eventos,
+					  })
+	return render(request,'bloggy.html',context)
+    #return render_to_response('bloggy.html', context_instance=RequestContext(request))
