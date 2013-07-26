f66a9e4d65248ebbcb6778b4fc6c31144750ac8d
guigarfr <guigarfr@gmail.com>
2013-07-26 13:52:22 +0200

    problem url
    contacto y bienvenida cambian url

--- site/noproblem/noproblem/problems/urls.py
+++ site/noproblem/noproblem/problems/urls.py
@@ -5,7 +5,7 @@ from noproblem.problems import views
 
 urlpatterns = patterns('',
                        # ex: /problems/
-                       url(r'^$', views.index, name='index'),
+                       url(r'^$', views.bienvenida, name='index'),
                        # ex: /problems/5/
                        url(r'^(?P<prob_id>\d+)/$', views.detail, name='detail'),
                        # ex: /problems/5/stats/
@@ -18,6 +18,5 @@ urlpatterns = patterns('',
                        url(r'^user_detail/$', views.user_detail, name='user_detail'),
                        # ex: /problems/tree/
                        url(r'^tree/(?P<cat_id>\d+)/$', views.tree, name='tree'),
-                       url(r'^bienvenida/$', views.bienvenida, name='bienvenida'),
-                       url(r'^bienvenida/contacto/$', views.contacto, name='contacto'),
+                       url(r'^contacto/$', views.contacto, name='contacto'),
 )
