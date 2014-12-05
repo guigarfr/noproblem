92356a1bc5dafdfd1f02dea05ce51d14d7af80c8
guigarfr <guigarfr@users.noreply.github.com>
2014-12-05 14:44:42 +0100

    problems.utils.dbqueries: corregido error concatenacion listas

--- site/noproblem/noproblem/problems/utils/dbqueries.py
+++ site/noproblem/noproblem/problems/utils/dbqueries.py
@@ -1,8 +1,8 @@
 from noproblem.problems.models import Problem, Solves
 
 def get_problems_solved(usr):
-	solved_ok_idlist = Solves.objects.filter(user=usr).values_list('prob',flat=True).distinct()
-	return Problem.objects.filter(id__in = solved_ok_idlist).all()
+	solved_idlist = Solves.objects.filter(user=usr).values_list('prob',flat=True).distinct()
+	return Problem.objects.filter(id__in = solved_idlist).all()
 	
 def get_problems_correct(usr):
 	solved_ok_idlist = Solves.objects.filter(user=usr).filter(is_correct=True).values_list('prob',flat=True).distinct()
@@ -10,9 +10,9 @@ def get_problems_correct(usr):
 	
 def get_problems_cansolve(usr):
 	all_problems = Problem.objects.all()
-	solved_problems = get_problems_correct(usr)
-	initial_problems = all_problems.filter(requirements=None)
-	nextproblems = initial_problems;
-	for i in solved_problems:
-		nextproblems = nextproblems + list(all_problems.filter(requirements=i).all())
-	return list(set(nextproblems) - set(solved_problems))
+	solved_ok_problems = get_problems_correct(usr)
+	initial_problems = all_problems.filter(requirements=None).all()
+	nextproblems = list(initial_problems)
+	for i in solved_ok_problems:
+		nextproblems = list(nextproblems) + list(all_problems.filter(requirements=i).all())
+	return list(set(nextproblems) - set(solved_ok_problems))
